03f769c0a826e7a584bc8f4af426313391abcd46
LittleCanOpener <hrafn8@gmail.com>
2022-12-02 10:55:21 +0000

    prepering to import all images

--- home.py
+++ home.py
@@ -1,3 +1,21 @@
 """
 The Classic Asteroid game 
 """
+import pygame
+
+"""
+Screen width & height
+"""
+sw = 800
+sh = 800
+"""
+Importing images onto the screen.
+"""
+bg = pygame.image.load('#')
+player = pygame.image.load('./assets/images/spaceship.png')
+asteroid = pygame.image.load('./assets/images/#')
+asteroid = pygame.image.load('./assets/images/#')
+asteroid = pygame.image.load('./assets/images/#')
+asteroid = pygame.image.load('./assets/images/#')
+asteroid = pygame.image.load('./assets/images/#')
+asteroid = pygame.image.load('./assets/images/#')
