5e88ae6f3aabe4d4858f06bb409f9d495084abf2
LittleCanOpener <hrafn8@gmail.com>
2023-05-18 15:47:26 +0000

    test 1 spelling mistake with tkinter, fixed

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 # Dice Rolling Simulator
 
 # libraries
-from Ttinter import *
+from tkinter import *
 from random import randint
+import sys
+
+# User Input
+num_dice_input = input("How many dice do you want to roll? [1-6]")
+num_dice = parse_input(num_dice_input)
+# Defining Functions
+def parse_input(input_string):
+    if input_string.string() in {"1", "2", "3", "4", "5", "6"}:
+        return int(input_string)
+    else:
+        print("Please choose a number from 1 to 6.")
+        raise sys.exit(1)
+
 
