8824c8c42275a018ca9c30476aaca58b409f2e9c
LittleCanOpener <hrafn8@gmail.com>
2023-05-18 15:54:29 +0000

    its suppose to be strip.. I hate python

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ import sys
 # User Input
 num_dice_input = input("How many dice do you want to roll? [1-6] :")
 num_dice = parse_input(num_dice_input)
-
 # Defining Functions
 def parse_input(input_string):
-    if input_string.string() in {"1", "2", "3", "4", "5", "6"}:
+    if input_string.strip() in {"1", "2", "3", "4", "5", "6"}:
         return int(input_string)
     else:
         print("Please choose a number from 1 to 6.")
