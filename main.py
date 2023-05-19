16c3d450aab53576d165f39c477c4746f8bad303
LittleCanOpener <hrafn8@gmail.com>
2023-05-19 14:17:06 +0000

    added a small validation

--- main.py
+++ main.py
@@ -47,10 +47,17 @@ DICE_ART = {
     ),
 }
 
+
+
+
 dice = []
 total = 0
-num_of_dice = int(input("How many dice do you want to roll? [1-6] "))
-
+while True:
+    try:
+        num_of_dice = int(input("How many dice do you want to roll? [1-6] "))
+        break
+    except ValueError:
+        print("Please Choose a number")
 
 for die in range(num_of_dice):
     dice.append(random.randint(1, 6))
