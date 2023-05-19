9b55e4781bf735606f93473209fae8ce437b189d
LittleCanOpener <hrafn8@gmail.com>
2023-05-19 13:58:51 +0000

    fixed the problem with the total

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 # Dice Rolling Simulator
-
 # libraries
 import random
-import sys
 # Dice art by Leodanis Pozo Ramos.
 DICE_ART = {
     1: (
@@ -48,9 +46,6 @@ DICE_ART = {
         "└─────────┘",
     ),
 }
-DIE_HEIGHT = len(DICE_ART[1])
-DIE_WIDTH = len(DICE_ART[1][0])
-DIE_FACE_SEPARATOR = " "
 
 dice = []
 total = 0
@@ -60,11 +55,11 @@ num_of_dice = int(input("How many dice do you want to roll? [1-6] "))
 for die in range(num_of_dice):
     dice.append(random.randint(1, 6))
 
-for line in range(9):
+for line in range(5):
     for die in dice:
         print(DICE_ART.get(die)[line], end="")
     print()
 
 for die in dice:
     total += die
-print(f"total: {total}")
+print(f"Total: {total}")
