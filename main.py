089e3f0342d8c3fd4af3c7ad382717ab5c1ac16a
LittleCanOpener <hrafn8@gmail.com>
2023-05-20 12:47:23 +0000

    trying to add more validation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Dice Rolling Simulator
 # libraries
 import random
+import sys
 # Dice art by Leodanis Pozo Ramos.
 DICE_ART = {
     1: (
@@ -51,10 +52,22 @@ dice = []
 total = 0
 while True:
     try:
-        num_of_dice = int(input("How many dice do you want to roll? "))
-        break
+        num_of_dice = int(input("How many dice do you want to roll [1-10]? "))
+        if not num_of_dice:
+            print("Please do not leave the input blank")
+            continue
+        if num_of_dice >= above:
+            break
+        else:
+            print("Please Choose a number")
     except ValueError:
         print("Please Choose a number")
+
+
+if num_of_dice <= 10:
+    print("Please Choose a number from [1-10]")
+else:
+     sys.exit(1)
 
 for die in range(num_of_dice):
     dice.append(random.randint(1, 6))
